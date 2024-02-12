4edd3c939ca265058b52c140abd1488259718dfd
codehustle18 <prashantmahato77@gmail.com>
2024-02-12 11:21:12 +0545

    Production Grade React Components

--- megaBlogWithAppWrite/src/components/index.js
+++ megaBlogWithAppWrite/src/components/index.js
@@ -1,4 +1,7 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Logo from "./Logo";
+import Container from "./container/Container";
+import Logout from "./Header/Logout";
 
-export { Header, Footer };
+export { Header, Footer, Logo, Container, Logout };
