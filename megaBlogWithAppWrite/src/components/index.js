59ed795ccb0ea0f224a3c490dff487ca01e0ab21
codehustle18 <prashantmahato77@gmail.com>
2024-02-13 12:40:20 +0545

    slugs and form for RTE

--- megaBlogWithAppWrite/src/components/index.js
+++ megaBlogWithAppWrite/src/components/index.js
@@ -3,5 +3,6 @@ import Footer from "./Footer/Footer";
 import Logo from "./Logo";
 import Container from "./container/Container";
 import Logout from "./Header/Logout";
+import RTE from "./RTE";
 
-export { Header, Footer, Logo, Container, Logout };
+export { Header, Footer, Logo, Container, Logout, RTE };
