664c41574830b6a20a559d72c669ed4f66d6b40e
codehustle18 <prashantmahato77@gmail.com>
2024-02-18 20:49:17 +0545

    imageslider

--- imageSlider/src/App.jsx
+++ imageSlider/src/App.jsx
@@ -7,20 +7,19 @@ function App() {
     "https://images.pexels.com/photos/1112080/pexels-photo-1112080.jpeg?auto=compress&cs=tinysrgb&w=1200",
     "https://images.pexels.com/photos/2252584/pexels-photo-2252584.jpeg?auto=compress&cs=tinysrgb&w=1200",
     "https://images.pexels.com/photos/2252618/pexels-photo-2252618.jpeg?auto=compress&cs=tinysrgb&w=1200",
-    "https://images.pexels.com/photos/1443867/pexels-photo-1443867.jpeg?auto=compress&cs=tinysrgb&w=1200",
   ];
   const [curImg, setCurImg] = useState(0);
   const nextImage = useCallback(() => {
-    setCurImg((prev) => (curImg === images.length - 1 ? 0 : prev + 1));
+    setCurImg((prev) => (prev === images.length - 1 ? 0 : prev + 1));
   }, [curImg, setCurImg, images]);
   const prevImg = useCallback(() => {
-    setCurImg((prev) => (curImg === 0 ? images.length - 1 : prev - 1));
+    setCurImg((prev) => (prev === 0 ? images.length - 1 : prev - 1));
   }, [curImg, setCurImg, images]);
 
   return (
     <>
       <div className="w-full flex justify-center items-center flex-col mt-8">
-        <h1>Image Slider</h1>
+        <h1 className="text-gray-400">Image Slider</h1>
         <div className="flex justify-center items-center w-1/2 mt-8">
           <button
             onClick={prevImg}
